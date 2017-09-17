8671cb4f440d1bcba9182e85561adac6d4321e36
sjau <github.com@sjau.ch>
2017-09-17 18:08:30 +0200

    Added Master PDF Editor

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nix-expressions
 Just a small repository containing additional nix-expressions that are not in NixOS
+
+# Guide for Packaging Binaries
+
+https://nixos.wiki/wiki/Packaging_Binaries
---
+++ master-pdf-editor.nix
@@ -0,0 +1,45 @@
+{ stdenv, lib, qt5, sane-backends, makeWrapper, fetchurl }:
+stdenv.mkDerivation rec {
+    name = "master-pdf-editor--${version}";
+    version = "4.3.10";
+
+    src = fetchurl {
+        url = "http://get.code-industry.net/public/master-pdf-editor-${version}_qt5.amd64.deb";
+        sha256 = "1z26qjhbiyz33rm7mp8ycgl5ka0v3v5lv5i5v0b5mx35arvx2zzy";
+    };
+    sourceRoot = ".";
+    unpackCmd = ''
+        ar p "$src" data.tar.xz | tar xJ
+    '';
+
+    buildPhase = ":";       # nothing to build
+    libPath = lib.makeLibraryPath [
+        qt5.qtbase          # libQt5PrintSupport.so.5
+        qt5.qtsvg           # libQt5Svg.so.5
+        stdenv.cc.cc.lib    # libstdc++.so.6
+        sane-backends       # libsane.so.1
+    ];
+    installPhase = ''
+        mkdir -p $out/bin
+        cp -R usr/share opt $out/
+        # fix the path in the desktop file
+        substituteInPlace \
+            $out/share/applications/masterpdfeditor4.desktop \
+            --replace /opt/ $out/opt/
+        # symlink the binary to bin/
+        ln -s $out/opt/master-pdf-editor-4/masterpdfeditor4 $out/bin/masterpdfeditor4
+    '';
+    postFixup = ''
+        patchelf \
+            --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+            --set-rpath "${libPath}" \
+            $out/opt/master-pdf-editor-4/masterpdfeditor4
+    '';
+
+    meta = with stdenv.lib; {
+        homepage = https://code-industry.net/masterpdfeditor/;
+        description = "a multifunctional PDF Editor";
+        platforms = platforms.linux;
+        maintainers = [ your_name ];
+    };
+}
