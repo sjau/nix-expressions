be4659bb3a49db1175f2ba161c56e081ef82d957
sjau <github.com@sjau.ch>
2017-09-28 16:49:13 +0200

    switching to 4-spaces indents

--- getTechDetails.nix
+++ getTechDetails.nix
@@ -1,18 +1,18 @@
 { stdenv, writeScriptBin }:
 
 writeScriptBin "getTechDetails" ''
-  #!${stdenv.shell}
+    #!${stdenv.shell}
 
-  # Small script to get the required technical details
-  # when filing a bug report against NixOS
+    # Small script to get the required technical details
+    # when filing a bug report against NixOS
 
-  nixosVer=$(nixos-version)
-  nixosEnv=$(nix-env --version)
-  nixosPkgs=$(nix-instantiate --eval '<nixpkgs>' -A lib.nixpkgsVersion)
-  nixosSand=$(grep build-use-sandbox /etc/nix/nix.conf)
+    nixosVer=$(nixos-version)
+    nixosEnv=$(nix-env --version)
+    nixosPkgs=$(nix-instantiate --eval '<nixpkgs>' -A lib.nixpkgsVersion)
+    nixosSand=$(grep build-use-sandbox /etc/nix/nix.conf)
 
-  printf '%s\n\n`%s`\n\n' '* System: (NixOS: `nixos-version`, Ubuntu/Fedora: `lsb_release -a`, ...)'            "''${nixosVer}"
-  printf '%s\n\n`%s`\n\n' '* Nix version: (run `nix-env --version`)'                                            "''${nixosEnv}"
-  printf '%s\n\n`%s`\n\n' '* Nixpkgs version: (run `nix-instantiate --eval "<nixpkgs>" -A lib.nixpkgsVersion`)' "''${nixosPkgs}"
-  printf '%s\n\n`%s`\n'   '* Sandboxing enabled: (run `grep build-use-sandbox /etc/nix/nix.conf`)'              "''${nixosSand}"
+    printf '%s\n\n`%s`\n\n' '* System: (NixOS: `nixos-version`, Ubuntu/Fedora: `lsb_release -a`, ...)'            "''${nixosVer}"
+    printf '%s\n\n`%s`\n\n' '* Nix version: (run `nix-env --version`)'                                            "''${nixosEnv}"
+    printf '%s\n\n`%s`\n\n' '* Nixpkgs version: (run `nix-instantiate --eval "<nixpkgs>" -A lib.nixpkgsVersion`)' "''${nixosPkgs}"
+    printf '%s\n\n`%s`\n'   '* Sandboxing enabled: (run `grep build-use-sandbox /etc/nix/nix.conf`)'              "''${nixosSand}"
 ''
