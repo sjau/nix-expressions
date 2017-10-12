40aeb3ce01573c3bdabb51d5f63783559a600b06
sjau <github.com@sjau.ch>
2017-10-12 10:56:24 +0200

    added custom iso

---
+++ customIso.nix
@@ -0,0 +1,106 @@
+{config, pkgs, ...}:
+{
+    imports = [
+        <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-base.nix>
+        # Provide an initial copy of the NixOS channel so that the user
+        # doesn't need to run "nix-channel --update" first.
+        <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+        ./customIsoFiles/files.nix
+    ];
+
+    # Enable ZFS (unstable needed for native zfs encryption)
+    boot.supportedFilesystems = [ "zfs" ];
+    boot.zfs.enableUnstable = true;
+
+    # Select internationalisation properties.
+    i18n = {
+        consoleFont = "Lat2-Terminus16";
+        consoleKeyMap = "sg-latin1";    # Swiss German keyboard layout
+        defaultLocale = "en_US.UTF-8";
+    };
+
+#    services.xserver = {
+#        enable = true;
+#        layout = "ch";
+#        xkbOptions = "eurosign:e";
+#        # Automatically login as root.
+#        displayManager.slim = {
+#            enable = true;
+#            defaultUser = "root";
+#            autoLogin = true;
+#        };
+#        desktopManager.plasma5 = {
+#            enable = true;
+#            enableQt4Support = false;
+#        };
+#        # Enable touchpad support for many laptops.
+#        synaptics.enable = true;
+#    };
+
+    # Use KDE5 unstable
+#    nixpkgs.config.packageOverrides = super: let self = super.pkgs; in {
+#          plasma5_stable = self.plasma5_latest;
+#        kdeApps_stable = self.kdeApps_latest;
+#    };
+
+    # KDE complains if power management is disabled (to be precise, if
+    # there is no power management backend such as upower).
+#    powerManagement.enable = true;
+
+
+    # Start the X server by default.
+    services.xserver.autorun = false;
+
+    # Provide networkmanager for easy wireless configuration.
+    networking.networkmanager.enable = true;
+    networking.wireless.enable = false;
+
+
+    # Time.
+    time.timeZone = "Europe/Zurich";
+
+    # State Version - mostly for bases
+    system.stateVersion = "17.09";
+    system.activationScripts.installerDesktop = let
+        desktopFile = pkgs.writeText "nixos-manual.desktop" ''
+        [Desktop Entry]
+        Version=1.0
+        Type=Application
+        Name=NixOS Manual
+        Exec=firefox ${config.system.build.manual.manual}/share/doc/nixos/index.html
+        Icon=text-html
+        '';
+    in ''
+        mkdir -p /root/Desktop
+        ln -sfT ${desktopFile} /root/Desktop/nixos-manual.desktop
+        ln -sfT ${pkgs.konsole}/share/applications/org.kde.konsole.desktop /root/Desktop/org.kde.konsole.desktop
+        ln -sfT ${pkgs.gparted}/share/applications/gparted.desktop /root/Desktop/gparted.desktop
+    '';
+
+    environment.files.root = {
+      root = "/root";
+      files = {
+        ".tmux.conf".source = "${./customIsoFiles/tmux.conf}";
+        "min_conf.nix".source = "${./customIsoFiles/min_conf.nix}";
+        "installZFS" = {
+            mode = "a+rwx";
+            user = "root";
+            source = "${./customIsoFiles/installZFS}";
+        };
+        ".fullInstall" = {
+            mode = "a+rwx";
+            user = "root";
+            source = "${./customIsoFiles/fullInstall}";
+        };
+      };
+    };
+
+    # List of packages that gets installed....
+    environment.systemPackages = with pkgs; [
+        cryptsetup
+        htop
+        gparted
+        kate
+        tmux
+    ];
+}
---
+++ customIsoFiles/min_conf.nix
@@ -0,0 +1,80 @@
+{ config, pkgs, ... }:
+
+{
+    imports =
+        [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+        ];
+
+    # Add more filesystems
+    boot.supportedFilesystems = [ "zfs" ];
+    boot.zfs.enableUnstable = true;
+    services.zfs.autoSnapshot = {
+        enable = true;
+    };
+
+    # Use the GRUB 2 boot loader.
+    boot.loader.grub.enable = true;
+    boot.loader.grub.version = 2;
+    boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+    # Trust hydra. Needed for one-click installations.
+    nix.trustedBinaryCaches = [ "http://hydra.nixos.org" ];
+
+    # Setup networking
+    networking = {
+        hostName = "freshNixos"; # Define your hostname.
+        hostId = "bac8c473";
+        #  enable = true;  # Enables wireless. Disable when using network manager
+        networkmanager.enable = true;
+    };
+
+    # Select internationalisation properties.
+    i18n = {
+        consoleFont = "Lat2-Terminus16";
+        consoleKeyMap = "sg-latin1";
+        defaultLocale = "en_US.UTF-8";
+    };
+
+    # Enable the OpenSSH daemon.
+    services.openssh = {
+        enable = true;
+        permitRootLogin = "yes";
+    };
+
+    # Enable ntp or rather timesyncd
+    services.timesyncd = {
+        enable = true;
+        servers = [ "0.ch.pool.ntp.org" "1.ch.pool.ntp.org" "2.ch.pool.ntp.org" "3.ch.pool.ntp.org" ];
+    };
+
+    # Time.
+    time.timeZone = "Europe/Zurich";
+
+    # Add the NixOS Manual on virtual console 8
+    services.nixosManual.showManual = true;
+
+    # Setup nano
+    programs.nano.nanorc = ''
+        set nowrap
+        set tabstospaces
+        set tabsize 4
+        set const
+        # include /usr/share/nano/sh.nanorc
+    '';
+
+    # The NixOS release to be compatible with for stateful data such as databases.
+    system.stateVersion = "17.09";
+
+    nixpkgs.config.allowUnfree = true;
+
+    # List packages installed in system profile.
+    environment.systemPackages = with pkgs; [
+        cryptsetup
+        curl
+        htop
+        gparted
+        kate
+        tmux
+    ];
+}
