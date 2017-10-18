aca197578f920689637d11fe56f848fce7f9fbb3
sjau <github.com@sjau.ch>
2017-10-18 12:24:21 +0200

    fixing script for nixos

--- autoResilver.nix
+++ autoResilver.nix
@@ -8,7 +8,7 @@ writeScriptBin "autoResilver" ''
     devices[0]="usb-TOSHIBA_External_USB_3.0_20170612010552F-0:0"
     devices[1]="usb-TOSHIBA_External_USB_3.0_2012110725463-0:0"
 
-    for i in "${devices[@]}"; do
+    for i in "$devices[@]"; do
         poolStatus=$(zpool status | grep "${i}")
         if [[ -b "/dev/disk/by-id/${i}" && ( "${poolStatus}" == *"UNAVAIL"* || "${poolStatus}" == *"OFFLINE"* ) ]]; then
             # Device exists on the system, but is not in zpool; set it to online
