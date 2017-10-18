5781479daf0d4dc42e6bb001c393ad88391c673f
sjau <github.com@sjau.ch>
2017-10-18 12:45:01 +0200

    make autoResilver more generic

--- autoResilver.nix
+++ autoResilver.nix
@@ -3,16 +3,17 @@
 writeScriptBin "autoResilver" ''
     #!${stdenv.shell}
 
-    poolName="tank"
+    # Small script to auto-resilver devices that belong to pool
+    # Usage: autoResilver "pool name" "device id"
+    # You could add a system cron job like
+    #     "*/5 * * * autoResilver 'tank' 'usb-TOSHIBA_External_USB_3.0_20170612010552F-0:0'"
 
-    devices[0]="usb-TOSHIBA_External_USB_3.0_20170612010552F-0:0"
-    devices[1]="usb-TOSHIBA_External_USB_3.0_2012110725463-0:0"
+    poolName="$1"
+    device="$2"
 
-    for i in "$devices[@]"; do
-        poolStatus=$(zpool status | grep "${i}")
-        if [[ -b "/dev/disk/by-id/${i}" && ( "${poolStatus}" == *"UNAVAIL"* || "${poolStatus}" == *"OFFLINE"* ) ]]; then
-            # Device exists on the system, but is not in zpool; set it to online
-            zpool online "${poolName}" "/dev/disk/by-id/${i}"
-        fi
-    done
+    poolStatus=$(zpool status | grep "$i")
+    if [[ -b "/dev/disk/by-id/$i" && ( "$poolStatus" == *"UNAVAIL"* || "$poolStatus" == *"OFFLINE"* ) ]]; then
+        # Device exists on the system, but is not in zpool; set it to online
+        zpool online "$poolName" "/dev/disk/by-id/$i"
+    fi
 ''
