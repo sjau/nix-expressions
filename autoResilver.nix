4baa3c1788bbecd847d117d930cf8a0fba3e23da
sjau <github.com@sjau.ch>
2017-10-18 14:00:10 +0200

    adding stopResilver

--- autoResilver.nix
+++ autoResilver.nix
@@ -4,16 +4,18 @@ writeScriptBin "autoResilver" ''
     #!${stdenv.shell}
 
     # Small script to auto-resilver devices that belong to pool
-    # Usage: autoResilver "pool name" "device id"
+    # Usage: autoResilver "pool name" "device1 device2 device3"
     # You could add a system cron job like
-    #     "*/5 * * * root autoResilver 'tank' 'usb-TOSHIBA_External_USB_3.0_20170612010552F-0:0'"
+    #     "*/5 * * * root autoResilver 'tank' 'usb-TOSHIBA_External_USB_3.0_20170612010552F-0:0 usb-TOSHIBA_External_USB_3.0_2012110725463-0:0'"
 
     poolName="$1"
-    device="$2"
+    read -r -a device <<<"$2"
 
-    poolStatus=$(zpool status | grep "$device")
-    if [[ -b "/dev/disk/by-id/$device" && ( "$poolStatus" == *"UNAVAIL"* || "$poolStatus" == *"OFFLINE"* ) ]]; then
-        # Device exists on the system, but is not in zpool; set it to online
-        zpool online "$poolName" "/dev/disk/by-id/$device"
-    fi
+    for i in "$device[@]"; do
+        poolStatus=$(zpool status | grep "$i")
+        if [[ -b "/dev/disk/by-id/$i" && ( "$poolStatus" == *"UNAVAIL"* || "$poolStatus" == *"OFFLINE"* ) ]]; then
+            # Device exists on the system, but is not in zpool; set it to online
+            zpool online "$poolName" "/dev/disk/by-id/$i"
+        fi
+    done
 ''
---
+++ stopResilver.nix
@@ -0,0 +1,21 @@
+{ stdenv, writeScriptBin }:
+
+writeScriptBin "stopResilver" ''
+    #!${stdenv.shell}
+
+    # Small script to stop auto-resilver/mirroring devices that belong to the pool
+    # Usage: stopResilver "pool name" "device1 device2 device3"
+    # Best to use it with a systemd unit file that runs before shutdown.
+    # Setting the external devices to "OFFLINE" causes faster re-silvering (maybe)
+
+    poolName="$1"
+    read -r -a device <<<"$2"
+
+    for i in "$device[@]"; do
+        poolStatus=$(zpool status | grep "$i")
+        if [[ -b "/dev/disk/by-id/$i" && "$poolStatus" == *"ONLINE"* ]]; then
+            # Device exists on the system and is in zpool; set it to offline
+            zpool offline "$poolName" "/dev/disk/by-id/$i"
+        fi
+    done
+''
