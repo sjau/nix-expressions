d5f5405cc3c46ab72a258f8b78f32fca226ef820
sjau <github.com@sjau.ch>
2017-11-12 15:37:54 +0100

    changed to append info

--- wgDebug.nix
+++ wgDebug.nix
@@ -13,6 +13,6 @@ writeScriptBin "wgDebug" ''
     printf 'cat /etc/host.conf\n' >> "/tmp/wginfo.txt"
     cat "/etc/host.conf" >> "/tmp/wginfo.txt"
     printf '\n\n\n' >> "/tmp/wginfo.txt"
-    printf 'journalctl -b -u wireguard-wg_home\n' > "/tmp/wginfo.txt"
+    printf 'journalctl -b -u wireguard-wg_home\n' >> "/tmp/wginfo.txt"
     journalctl -b -u wireguard-wg_home >> "/tmp/wginfo.txt"
 ''
