a1aa9e1a8e06be8aa790798566b950f4f7677bca
sjau <github.com@sjau.ch>
2017-11-12 13:49:59 +0100

    expanding catResolv

--- catResolv.nix
+++ catResolv.nix
@@ -5,4 +5,6 @@ writeScriptBin "catResolv" ''
 
     # Small script that cats the /etc/resolv.conf file to /tmp/resolv.boot
     cat "/etc/resolv.conf" > "/tmp/resolv.boot"
+    cat "/etc/nsswitch.conf" > "/tmp/nsswitch.boot"
+    cat "/etc/host.conf" > "/tmp/host.boot"
 ''
