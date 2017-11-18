fc82848bf8e23ada10473c4591a2196f207fd5aa
sjau <github.com@sjau.ch>
2017-11-18 12:41:25 +0100

    updated getTechDetails to new instructions

--- getTechDetails.nix
+++ getTechDetails.nix
@@ -1,4 +1,4 @@
-{ stdenv, writeScriptBin }:
+{ stdenv, writeScriptBin, nix-info }:
 
 writeScriptBin "getTechDetails" ''
     #!${stdenv.shell}
@@ -6,13 +6,15 @@ writeScriptBin "getTechDetails" ''
     # Small script to get the required technical details
     # when filing a bug report against NixOS
 
-    nixosVer=$(nixos-version)
-    nixosEnv=$(nix-env --version)
-    nixosPkgs=$(nix-instantiate --eval '<nixpkgs>' -A lib.nixpkgsVersion)
-    nixosSand=$(grep build-use-sandbox /etc/nix/nix.conf)
+#    nixosVer=$(nixos-version)
+#    nixosEnv=$(nix-env --version)
+#    nixosPkgs=$(nix-instantiate --eval '<nixpkgs>' -A lib.nixpkgsVersion)
+#    nixosSand=$(grep build-use-sandbox /etc/nix/nix.conf)
+    nixInfo=$(nix-info -m)
 
-    printf '%s\n\n`%s`\n\n' '* System: (NixOS: `nixos-version`, Ubuntu/Fedora: `lsb_release -a`, ...)'            "''${nixosVer}"
-    printf '%s\n\n`%s`\n\n' '* Nix version: (run `nix-env --version`)'                                            "''${nixosEnv}"
-    printf '%s\n\n`%s`\n\n' '* Nixpkgs version: (run `nix-instantiate --eval "<nixpkgs>" -A lib.nixpkgsVersion`)' "''${nixosPkgs}"
-    printf '%s\n\n`%s`\n'   '* Sandboxing enabled: (run `grep build-use-sandbox /etc/nix/nix.conf`)'              "''${nixosSand}"
+#    printf '%s\n\n`%s`\n\n' '* System: (NixOS: `nixos-version`, Ubuntu/Fedora: `lsb_release -a`, ...)'            "''${nixosVer}"
+#    printf '%s\n\n`%s`\n\n' '* Nix version: (run `nix-env --version`)'                                            "''${nixosEnv}"
+#    printf '%s\n\n`%s`\n\n' '* Nixpkgs version: (run `nix-instantiate --eval "<nixpkgs>" -A lib.nixpkgsVersion`)' "''${nixosPkgs}"
+#    printf '%s\n\n`%s`\n'   '* Sandboxing enabled: (run `grep build-use-sandbox /etc/nix/nix.conf`)'              "''${nixosSand}"
+    printf '%s\n\n'          "${nixInfo}"
 ''
