3c71a49a94c9e8b9f96a1e3be8c0881b6c5c2878
sjau <github.com@sjau.ch>
2017-11-18 19:28:09 +0100

    fixing nixinfo

--- getTechDetails.nix
+++ getTechDetails.nix
@@ -10,11 +10,11 @@ writeScriptBin "getTechDetails" ''
 #    nixosEnv=$(nix-env --version)
 #    nixosPkgs=$(nix-instantiate --eval '<nixpkgs>' -A lib.nixpkgsVersion)
 #    nixosSand=$(grep build-use-sandbox /etc/nix/nix.conf)
-    nixInfo=$(nix-info -m)
+    nixosInfo=$(nix-info -m)
 
 #    printf '%s\n\n`%s`\n\n' '* System: (NixOS: `nixos-version`, Ubuntu/Fedora: `lsb_release -a`, ...)'            "''${nixosVer}"
 #    printf '%s\n\n`%s`\n\n' '* Nix version: (run `nix-env --version`)'                                            "''${nixosEnv}"
 #    printf '%s\n\n`%s`\n\n' '* Nixpkgs version: (run `nix-instantiate --eval "<nixpkgs>" -A lib.nixpkgsVersion`)' "''${nixosPkgs}"
 #    printf '%s\n\n`%s`\n'   '* Sandboxing enabled: (run `grep build-use-sandbox /etc/nix/nix.conf`)'              "''${nixosSand}"
-    printf '%s\n\n'          "''${nixInfo}"
+    printf '%s\n\n'          "''${nixosInfo}"
 ''
