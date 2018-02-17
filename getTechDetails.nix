c5f746abc7a427be3f8767fa4d33986029d1c766
sjau <github.com@sjau.ch>
2018-02-17 13:13:10 +0100

    changes

--- getTechDetails.nix
+++ getTechDetails.nix
@@ -6,15 +6,7 @@ writeScriptBin "getTechDetails" ''
     # Small script to get the required technical details
     # when filing a bug report against NixOS
 
-#    nixosVer=$(nixos-version)
-#    nixosEnv=$(nix-env --version)
-#    nixosPkgs=$(nix-instantiate --eval '<nixpkgs>' -A lib.nixpkgsVersion)
-#    nixosSand=$(grep build-use-sandbox /etc/nix/nix.conf)
-    nixosInfo=$(nix-info -m)
+    nixosInfo=$(${nix-info}/bin/nix-info -m)
 
-#    printf '%s\n\n`%s`\n\n' '* System: (NixOS: `nixos-version`, Ubuntu/Fedora: `lsb_release -a`, ...)'            "''${nixosVer}"
-#    printf '%s\n\n`%s`\n\n' '* Nix version: (run `nix-env --version`)'                                            "''${nixosEnv}"
-#    printf '%s\n\n`%s`\n\n' '* Nixpkgs version: (run `nix-instantiate --eval "<nixpkgs>" -A lib.nixpkgsVersion`)' "''${nixosPkgs}"
-#    printf '%s\n\n`%s`\n'   '* Sandboxing enabled: (run `grep build-use-sandbox /etc/nix/nix.conf`)'              "''${nixosSand}"
     printf '%s\n\n'          "''${nixosInfo}"
 ''
