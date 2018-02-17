a82b1e1616a0cc615687eb9b5aa49d019f358a30
sjau <github.com@sjau.ch>
2018-02-17 13:03:08 +0100

    proper youtube-dl nix path

--- getMusic.nix
+++ getMusic.nix
@@ -4,5 +4,5 @@ writeScriptBin "getMusic" ''
     #!${stdenv.shell}
 
     # Small script to download
-    cd "/home/hyper/Syncthing/Music" && youtube-dl -x "$1"
+    cd "/home/hyper/Syncthing/Music" && ${youtube-dl}/bin/youtube-dl -x "$1"
 ''
