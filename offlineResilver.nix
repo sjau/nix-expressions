14faab9210761981514e791dc0418dcadda5256f
hyper_ch <gitlab.com@sjau.ch>
2020-03-17 10:37:37 +0100

    fixing some errors

--- offlineResilver.nix
+++ offlineResilver.nix
@@ -18,11 +18,11 @@ writeScriptBin "stopResilver" ''
     for i in ''${devices[@]}; do
         poolStatus=$(zpool status)
         # Check if pool is being resilvered, if so, exit
-        [[ ${poolStatus} == *"currently being resilvered"* ]] && exit 0
+        [[ $poolStatus == *"currently being resilvered"* ]] && exit 0
         # Check if pool is being scrubbed, if so, exit
-        [[ ${poolStatus} == *"scrub in progress"* ]] && exit 0
+        [[ $poolStatus == *"scrub in progress"* ]] && exit 0
         # Check if device exists, if so, set it offline
-        if [[ -b "/dev/disk/by-id/$i" ]] && cd /hzpool offline "$poolName" "$i"
+        if [[ -b "/dev/disk/by-id/$i" ]] && cd zpool offline "$poolName" "$i"
         fi
     done
 ''
