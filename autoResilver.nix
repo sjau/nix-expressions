542b9842a3e312f8b6cb4bba9e10f0bc8029e530
hyper_ch <gitlab.com@sjau.ch>
2020-03-30 05:22:34 +0200

    make the lock dir variable

--- autoResilver.nix
+++ autoResilver.nix
@@ -9,6 +9,7 @@ writeScriptBin "autoResilver" ''
     #     "*/5 * * * * root autoResilver 'tank' 'usb-TOSHIBA_External_USB_3.0_20170612010552F-0:0, usb-TOSHIBA_External_USB_3.0_2012110725463-0:0'"
 
     poolName="$1"
+    lockDir="/var/tmp"
     IFS=', ' read -r -a devices <<< "$2"
 
     for i in ''${devices[@]}; do
@@ -16,12 +17,12 @@ writeScriptBin "autoResilver" ''
         curDate=$(date '+%Y-%m-%d')
         if [[ -b "/dev/disk/by-id/$i" && ( "$poolStatus" == *"UNAVAIL"* || "$poolStatus" == *"OFFLINE"* ) ]]; then
             # Check if there is a lockfile for this day to ensure it only happens once a day
-            if [[ ! -f "/var/tmp/autoResilver-$i" ]] && [[ ! -f "/var/tmp/autoResilver-$curDate-$i" ]]; then
+            if [[ ! -f "$lockDir/autoResilver-$i" ]] && [[ ! -f "$lockDir/autoResilver-$curDate-$i" ]]; then
                 # Device exists on the system, but is not in zpool and no lockfile; set it to online and create lockfile
-                zpool online "$poolName" "/dev/disk/by-id/$i" && touch "/var/tmp/autoResilver-$i" && touch "/var/tmp/autoResilver-$curDate-$i"
+                zpool online "$poolName" "/dev/disk/by-id/$i" && touch "$lockDir/autoResilver-$i" && touch "$lockDir/autoResilver-$curDate-$i"
             else
             # Check if file modification was more than 24h ago and delete it, if so, delete lock files
-                for f in "/var/tmp/autoResilver"*; do
+                for f in "$lockDir/autoResilver"*; do
                     (($(date +"%s") - $(stat -c "%Y" "$f") > 86400)) && rm "$f"
                 done
             fi
