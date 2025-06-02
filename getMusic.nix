f27e730f06e12c1aa20c7eccc71572b6f18b8d8b
sjau <github.com_2022.02.04_1643985031@sjau.ch>
2025-06-02 07:20:16 +0200

    Update getMusic.nix

--- getMusic.nix
+++ getMusic.nix
@@ -1,8 +1,8 @@
-{ stdenv, writeScriptBin, youtube-dl }:
+{ stdenv, writeScriptBin, yt-dlp, ffmpeg }:
 
 writeScriptBin "getMusic" ''
     #!${stdenv.shell}
 
     # Small script to download
-    cd "/home/hyper/Syncthing/Music" && ${youtube-dl}/bin/youtube-dl -x "$1"
+    cd "/home/hyper/Syncthing/Music" && ${yt-dlp}/bin/yt-dlp -x "$1"
 ''
